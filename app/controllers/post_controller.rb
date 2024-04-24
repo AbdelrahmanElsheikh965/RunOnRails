2ae56628b429a2e04c4c90cf4ad17886a2dc28de
AbdelrahmanElsheikh965 <abdoelsheikh892@gmail.com>
2024-04-24 21:41:17 +0200

    Prepared model, migration and views

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,5 +1,33 @@
-class PostController < ActionController::Base
+class PostController < ApplicationController
+
     def index
-    
+        
     end
+
+    def new
+        # @post = post.new
+    end
+    
+    def create
+    @post = post.new(title: "...", body: "...")
+
+    if @post.save
+        redirect_to @post
+    else
+        render :new, status: :unprocessable_entity
+    end
+    end
+
+    def edit
+        
+    end
+
+    def update
+        
+    end
+
+    def delete
+
+    end
+    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_22_132355) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_24_190858) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.string "description"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
